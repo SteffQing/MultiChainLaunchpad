fae76c92ffb283f9d75428dbf334c8ae1d295e72
SteffQing <steveola23@gmail.com>
2022-12-23 11:06:51 +0100

    data.js update Ido date

--- pages/api/data.js
+++ pages/api/data.js
@@ -8,10 +8,10 @@ export default async function handler(req, res) {
     description:
       "Texo Starter is a MultiChain Launchpad built for the interoperability of EVM Chains. Texo Starter takes advantage of the security, decentralization and smart contract functionality of EVM-compatible chains to build a secure prelaunch platform for NFTs and ERC20 tokens.",
     image: "/logo.png",
-    IDOdate: 1670903400,
+    IDOdate: 1671796800,
     accessType: "Public",
     salePrice: 1,
-    distributionDate: 1670989800,
+    distributionDate: 1672574400,
     targetSale: 50000,
   };
   res.status(200).json(data);
