640471dfe7ed80687bc8d8c89d590495d7d7d121
IbrahimIjai <64047050+IbrahimIjai@users.noreply.github.com>
2023-05-25 12:31:20 +0100

    Revert "work o clock"
    This reverts commit 7d6cea1b0e0b378589a6b14b1247f17640abc1d3.

--- Hackit/Asset/Index.js
+++ Hackit/Asset/Index.js
@@ -2,22 +2,22 @@ import AdobeIllustrator from "./AdobeIllustrator.png";
 import Amazon from "./Amazon.png";
 import Andriod from "./Andriod.png";
 import Angular from "./Angular.png";
-import Azure from "./Azure.png";
-import Blender from "./Blender.png";
-import Blockchain from "./Blockchain.png";
-import C_plus from "./C_plus.png";
-import C_sharp from "./C_sharp.png";
-import CommunityManager from "./CommunityManager.png";
+import Azure from "./Azure.png"
+import Blender from "./Blender.png"
+import Blockchain from "./Blockchain.png"
+import C_plus from "./C_plus.png"
+import  C_sharp from "./C_sharp.png"
+import  CommunityManager from "./CommunityManager.png";
 import CustomerService from "./CustomerService.png";
 import Dart from "./Dart.png";
-import Digital from "./DigitalMkt.png";
+import Digital from  "./DigitalMkt.png";
 import Figma from "./Figma.png";
 import Flutter from "./Flutter.png";
 import Github from "./Github.png";
 import Goland from "./Goland.png";
 import Html from "./Html.png";
 import IOSdev from "./IOSdev.png";
-import Java from "./Java.png";
+import Java from "./Java.png"
 import Javascript from "./Javascript.png";
 import Mongodb from "./Mongodb.png";
 import MySql from "./MySql.png";
@@ -27,155 +27,155 @@ import PhpLeveral from "./PhpLeveral.png";
 import PostgreSql from "./PostgreSql.png";
 import ProjectMgt from "./ProjectMgt.png";
 import Python from "./Python.png";
-import React from "./React.png";
+import React from "./React.png"
 import RubyonRails from "./RubyonRails.png";
 import Solidity from "./Solidity.png";
 import Typescript from "./Typescript.png";
 import Unity from "./Unity.png";
-import UnrealEngine from "./UnrealEngine.png";
+import UnrealEngine from "./UnrealEngine.png"
 export const Array = [
-  {
-    name: "Python",
-    Icon: Python,
-  },
-  {
-    name: "Amazon",
-    Icon: Amazon,
-  },
-  {
-    name: "Andriod",
-    Icon: Andriod,
-  },
-  {
-    name: "Angular",
-    Icon: Angular,
-  },
-  {
-    name: "Azure",
-    Icon: Azure,
-  },
-  {
-    name: "Blender",
-    Icon: Blender,
-  },
-  {
-    name: "Blockchain",
-    Icon: Blockchain,
-  },
-  {
-    name: "C_plus",
-    Icon: C_plus,
-  },
-  {
-    name: "C_sharp",
-    Icon: C_sharp,
-  },
-  {
-    name: "Dart",
-    Icon: Dart,
-  },
-  {
-    name: "Dart",
-    Icon: Dart,
-  },
-  {
-    name: "Figma",
-    Icon: Figma,
-  },
-  {
-    name: "Flutter",
-    Icon: Flutter,
-  },
-  {
-    name: "Github",
-    Icon: Github,
-  },
-  {
-    name: "Goland",
-    Icon: Goland,
-  },
-  {
-    name: "Html",
-    Icon: Html,
-  },
-  {
-    name: "IOSdev",
-    Icon: IOSdev,
-  },
-  {
-    name: "Java",
-    Icon: Java,
-  },
-  {
-    name: "Javascript",
-    Icon: Javascript,
-  },
-  {
-    name: "Mongodb",
-    Icon: Mongodb,
-  },
-  {
-    name: "MySql",
-    Icon: MySql,
-  },
-  {
-    name: "Node",
-    Icon: Node,
-  },
-  {
-    name: "Photoshop",
-    Icon: Photoshop,
-  },
-  {
-    name: "PHP Leveral",
-    Icon: PhpLeveral,
-  },
-  {
-    name: "PostgreSql",
-    Icon: PostgreSql,
-  },
-  {
-    name: "UnrealEngine",
-    Icon: UnrealEngine,
-  },
-  {
-    name: "Unity",
-    Icon: Unity,
-  },
-  {
-    name: "Typescript",
-    Icon: Typescript,
-  },
-  {
-    name: "Solidity",
-    Icon: Solidity,
-  },
-  {
-    name: "Ruby on Rails",
-    Icon: RubyonRails,
-  },
-  {
-    name: "React",
-    Icon: React,
-  },
-  {
-    name: "Adobe Illustrator",
-    Icon: AdobeIllustrator,
-  },
-  {
-    name: "Project Management",
-    Icon: ProjectMgt,
-  },
-  {
-    name: "Community Manager",
-    Icon: CommunityManager,
-  },
-  {
-    name: "Customer Service",
-    Icon: CustomerService,
-  },
-  {
-    name: "Digital Marketting",
-    Icon: Digital,
-  },
-];
+    {
+        name: "Adobe Illustrator",
+        Icon: AdobeIllustrator
+    },
+    {
+        name: "Amazon",
+        Icon: Amazon
+    },
+    {
+        name: "Andriod",
+        Icon: Andriod
+    },
+    {
+        name: "Angular",
+        Icon: Angular
+    },
+    {
+        name: "Azure",
+        Icon: Azure
+    },
+    {
+        name: "Blender",
+        Icon: Blender
+    },
+    {
+        name: "Blockchain",
+        Icon: Blockchain
+    },
+    {
+        name: "C_plus",
+        Icon: C_plus
+    },
+    {
+        name: "C_sharp",
+        Icon: C_sharp
+    },
+    {
+        name: "CommunityManager",
+        Icon: CommunityManager
+    },
+    {
+        name: "CustomerService",
+        Icon: CustomerService
+    },
+    {
+        name: "Dart",
+        Icon: Dart
+    },
+    {
+        name: "Dart",
+        Icon: Dart
+    },
+    {
+        name: "Digital Marketting",
+        Icon: Digital
+    },
+    {
+        name: "Figma",
+        Icon: Figma
+    },
+    {
+        name: "Flutter",
+        Icon: Flutter
+    },
+    {
+        name: "Github",
+        Icon: Github
+    },
+    {
+        name: "Goland",
+        Icon: Goland
+    },
+    {
+        name: "Html",
+        Icon: Html
+    },
+    {
+        name: "IOSdev",
+        Icon: IOSdev
+    },
+    {
+        name: "Java",
+        Icon: Java
+    },
+    {
+        name: "Javascript",
+        Icon: Javascript
+    },
+    {
+        name: "Mongodb",
+        Icon: Mongodb
+    },
+    {
+        name: "MySql",
+        Icon: MySql
+    },
+    {
+        name: "Node",
+        Icon: Node
+    },
+    {
+        name: "Photoshop",
+        Icon: Photoshop
+    },
+    {
+        name: "PHP Leveral",
+        Icon: PhpLeveral
+    },
+    {
+        name: "PostgreSql",
+        Icon: PostgreSql
+    },
+    {
+        name: "UnrealEngine",
+        Icon: UnrealEngine
+    },
+    {
+        name: "Unity",
+        Icon: Unity
+    },
+    {
+        name: "Typescript",
+        Icon: Typescript
+    },
+    {
+        name: "Solidity",
+        Icon: Solidity
+    },
+    {
+        name: "Ruby on Rails",
+        Icon: RubyonRails
+    },
+    {
+        name: "React",
+        Icon: React
+    },
+    {
+        name: "Python",
+        Icon: Python
+    },
+    {
+        name: "Project Management",
+        Icon: ProjectMgt
+    },
+]
